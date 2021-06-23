5bb359e28102e9db3907bd0acd75929b6c56e1fd
Leland Hayes <lelandkhayes13@gmail.com>
2021-06-22 20:11:38 -0400

    added function to js file that generates a prompt for user input about there password requirements

--- Develop/script.js
+++ Develop/script.js
@@ -30,6 +30,25 @@ var toUpper = function (x) {
 alpha2 = alpha.map(toUpper);
 
 
+// Start function to generate password
+function generatePassword() {
+    enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+    // First if statement for user validation 
+    if (!enter) {
+        alert("This needs a value");
+    } else if (enter < 8 || enter > 128) {
+        // User input prompts
+        enter = parseInt(prompt("You must choose between 8 and 128"));
+
+    } else {
+        // Continues once user input is validated
+        confirmNumber = confirm("Will this contain numbers?");
+        confirmCharacter = confirm("Will this contain special characters?");
+        confirmUppercase = confirm("Will this contain Uppercase letters?");
+        confirmLowercase = confirm("Will this contain Lowercase letters?");
+    };
+  }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
--- README.md
+++ README.md
@@ -5,4 +5,3 @@
     * A password generator for company employees that is soly based upon criteria they have chosen for their specific password.
     * Using complex HTML, CSS, and Java Script
 
-
